15518c69d68786ba51b1e811c7ed1bdd3fcdccd3
StevenDeFalco <stevencdefalco@gmail.com>
2023-06-21 12:08:31 -0400

    renamed to data.py

--- retrieve_data.py
+++
@@ -1,9 +0,0 @@
-import statsapi
-import pandas as pd
-
-def get_division_standings(league, division):
-    standings = pd.DataFrame(statsapi.standings_data(league)[division]['teams'])
-    standings = standings[['name', 'div_rank', 'gb', 'w', 'l', 'elim_num', 'wc_rank', 'wc_gb']]
-    return standings
-
-print(get_division_standings(104,204))
