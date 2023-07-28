47f9117443a5dc7a1d54a2349e92dd175b867984
StevenDeFalco <stevencdefalco@gmail.com>
2023-07-28 19:05:45 -0400

    provides functions to generate result and prediction tweet text

--- server/tweet_generator.py
+++ server/tweet_generator.py
@@ -1,28 +1,3 @@
-from dotenv import load_dotenv  # type: ignore
-import tweepy  # type: ignore
-import os
-
-load_dotenv()
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-
-def send_tweet(msg):
-    "general function to send a tweet from @predictmlb"
-    try:
-        api.update_status(msg)
-        print(f"Succcessfully tweeted: {msg}")
-    except tweepy.TweepyException as e:
-        print(f"Error sending tweet: {e}")
-
-
 def gen_prediction_tweet(
     pred_winner: str,
     pred_loser: str,
