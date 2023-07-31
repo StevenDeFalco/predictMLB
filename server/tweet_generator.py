0bc9aa09a89a98a55bfe8a6012e0a9d3bd42ada0
StevenDeFalco <stevencdefalco@gmail.com>
2023-07-31 09:08:54 -0400

    tweets ensure +/- signs are used with all ML odds provided for clarity

--- server/tweet_generator.py
+++ server/tweet_generator.py
@@ -1,20 +1,32 @@
+from typing import Optional
+
+
 def gen_prediction_tweet(
     pred_winner: str,
     pred_loser: str,
     time: str,
     venue: str,
-    winning_odds: str,
-    losing_odds: str,
-    winning_odds_bookmaker: str,
-    losing_odds_bookmaker: str,
+    winning_odds: Optional[str] = None,
+    losing_odds: Optional[str] = None,
+    winning_odds_bookmaker: Optional[str] = None,
+    losing_odds_bookmaker: Optional[str] = None,
 ) -> str:
-    # TODO: unique a.i. text generation
-    # placeholder tweet format below until implemented
-    msg = (
-        f"I predict the {pred_winner} ({winning_odds} on {winning_odds_bookmaker}) "
-        f"will defeat the {pred_loser} ({losing_odds} on {losing_odds_bookmaker}) "
-        f"at {time} when the two teams match up at {venue}."
-    )
+    if winning_odds and losing_odds:
+        losing_odds = (
+            ("+" + str(losing_odds)) if (int(losing_odds) > 100) else str(losing_odds)
+        )
+        winning_odds = (
+            ("+" + str(winning_odds))
+            if (int(winning_odds) > 100)
+            else str(winning_odds)
+        )
+    msg = f"I predict the {pred_winner} "
+    if winning_odds and winning_odds_bookmaker:
+        msg += f"({str(winning_odds)} on {winning_odds_bookmaker}) "
+    msg += f"will defeat the {pred_loser} "
+    if losing_odds and losing_odds_bookmaker:
+        msg += f"({str(losing_odds)} on {losing_odds_bookmaker}) "
+    msg += f"at {time} when the two teams match up at {venue}."
     return msg
 
 
@@ -29,12 +41,12 @@ def gen_result_tweet(
 ) -> str:
     msg = (
         f"Of yesterday's MLB games, I predicted {correct_wrong} correctly, "
-        f"and thus had a prediction accuracy of {percentage}. "
+        f"for a prediction accuracy of {percentage}. "
     )
     if is_upset:
         msg += (
             f"Among these predictions, I had correctly anticipated the {upset_winner} "
             f"(+{upset_winner_odds}) defeating the {upset_loser} ({upset_loser_odds}) "
-            f"(Odds are from the time of my tweeted prediction)."
+            f"(Odds from about 1 hour before game start)"
         )
     return msg
